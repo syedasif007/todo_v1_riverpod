9594c9fbf8a50db1d509aa4d8d5b1be50c7ab1be
Syed Asif Mahmood <11716526+syedasif007@users.noreply.github.com>
2026-08-02 17:51:33 +0600

    added go_router and implemented routes.

---
+++ lib/app/router/routes.dart
@@ -0,0 +1,48 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:todo_v1_riverpod/features/auth/presentation/pages/login_page.dart';
+
+import '../../features/auth/presentation/pages/auth_gate.dart';
+import '../../features/splash/presentation/splash_page.dart';
+
+extension Convert on String {
+  String get p => '/$this';
+}
+
+class Routes {
+  Routes._();
+
+  static const auth = 'auth';
+  static const splash = 'splash';
+  static const onboarding = 'onboarding';
+  static const login = 'login';
+  static const home = 'home';
+  static const profile = 'profile';
+  static const bookmarks = 'bookmarks';
+  static const settings = 'settings';
+  static const search = 'search';
+}
+
+final routerProvider = Provider<GoRouter>((ref) {
+  return GoRouter(
+    initialLocation: Routes.auth.p,
+    routes: [
+      GoRoute(path: '/', redirect: (context, state) => Routes.auth.p),
+      GoRoute(
+        path: Routes.auth.p,
+        name: Routes.auth,
+        builder: (context, state) => const AuthGate(),
+      ),
+      GoRoute(
+        path: Routes.splash.p,
+        name: Routes.splash,
+        builder: (context, state) => const SplashPage(),
+      ),
+      GoRoute(
+        path: Routes.login.p,
+        name: Routes.login,
+        builder: (context, state) => const LoginPage(),
+      ),
+    ],
+  );
+});
--- lib/main.dart
+++ lib/main.dart
@@ -2,22 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todo_v1_riverpod/features/auth/presentation/pages/auth_gate.dart';
 
+import 'app/router/routes.dart';
+
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  Widget build(BuildContext context, WidgetRef ref) {
+    final router = ref.watch(routerProvider);
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: true,
       title: 'Todo V1 Riverpod',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const AuthGate(),
+      routerConfig: router,
+      // builder: (context, _) {
+      //   return AuthGate(key: const ValueKey('auth-gate'));
+      // },
     );
   }
 }
