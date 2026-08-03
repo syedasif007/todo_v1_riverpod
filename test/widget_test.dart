9c82c3c2e117cc470682ecbc95b5c849f79a2bf5
Syed Asif Mahmood <11716526+syedasif007@users.noreply.github.com>
2026-08-03 18:06:28 +0600

    updated kotlin version, added explicit jdk path to gradle properties, added entitlements for ios, removed unused imports from widget test.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,9 +7,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import 'package:todo_v1_riverpod/main.dart';
 
 void main() {
   testWidgets('App builds and shows Tasks app bar', (
